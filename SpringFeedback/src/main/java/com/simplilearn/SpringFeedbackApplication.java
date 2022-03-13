59050aa3d2bb1d75639ad2bad70958fe51ac55ec
sk saud <soudshaik18@gmail.com>
2022-03-14 00:12:08 +0530

    User feedback

---
+++ SpringFeedback/src/main/java/com/simplilearn/SpringFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFeedbackApplication.class, args);
+	}
+
+}
